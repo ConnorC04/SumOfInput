0214c98977becf1980c0f53e8dfaf27ee7a26cfe
ConnorC04 <connorjcasey@iCloud.com>
2025-02-28 12:35:17 -0500

    All done

--- src/main/java/SumOfInput.java
+++ src/main/java/SumOfInput.java
@@ -10,18 +10,28 @@ public class SumOfInput{
 		int num = Integer.parseInt(scanner.nextLine());
 
 		System.out.println("Sum of all numbers between 1 and " + num + " is: " + sumNumbers(num));
+
+		System.out.println("Using Gaussian Wizardry: " + gaussian(num));
 	}
+
 		
 		
-	public static  int sumNumbers(int n){
+	public static  int gaussian(int n){
 			
 			int sum = 0;
-			
+
 			for (int i = 1; i <= n; i++){
-				sum = (i * (i + 1)) / 2;
-			}
-			
-			return sum;
+			sum = (i * (i + 1)) / 2;
+		}			
+		return sum;
+	}
+	public static int sumNumbers(int j){
+		int sumNum = 0;
+		
+		for (int k = 0; k <= j; k++){
+			sumNum = sumNum + k;
 		}
+		return sumNum;
+	}
 
 }
